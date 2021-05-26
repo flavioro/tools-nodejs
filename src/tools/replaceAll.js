8b8524ce53fc2f03e167c10aa22fdc176f4c2ab6
flavioro <flavioro@gmail.com>
2021-05-26 09:53:17 -0300

    Correction in replaceAll to module.exports

--- src/tools/replaceAll.js
+++ src/tools/replaceAll.js
@@ -1,4 +1,6 @@
-exports.ReplaceAll = function (string, search, replace) {
+// exports.ReplaceAll = function (string, search, replace) {
+// const ReplaceAll = function (string, search, replace) { //error
+function ReplaceAll (string, search, replace) { // error
   const stringNew = String(string).split(search).join(replace)
 
   return stringNew
@@ -7,3 +9,4 @@ exports.ReplaceAll = function (string, search, replace) {
 // console.log(ReplaceAll('Do more', 'more', 'more and more'))
 
 // module.exports = ReplaceAll
+module.exports.ReplaceAll = ReplaceAll

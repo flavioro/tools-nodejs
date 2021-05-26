a3c41daf3962d00ea5419f1f94209a3b6bbd1341
flavioro <flavioro@gmail.com>
2021-05-26 15:59:37 -0300

    Correction index.js to validFileExist and search replace text

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-const fs = require('fs')
 const fileExists = require('./tools/checkFile')
-// import fs from 'fs'
 const ReplaceAll = require('./tools/replaceAll')
 
 const replaceTextFile = () => {
   console.log(ReplaceAll.ReplaceAll('Do more', 'more', 'more and more'))
-
-  // const strReplace = ReplaceAll.ReplaceAll
-  // console.log(strReplace)
-  // // let txtReplace = new strReplace()
-  // let txtReplace = new strReplace
-  // console.log(txtReplace)
 }
 
 async function validFile () {
